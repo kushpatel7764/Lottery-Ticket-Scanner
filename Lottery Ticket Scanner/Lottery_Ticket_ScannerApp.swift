6b654ac58138220b368d2b8d76e56b0b3dcd11c7
Kush Patel <kushpatelrp1234@gmail.com>
2026-04-16 12:34:13 -0400

    Initial Commit

---
+++ Lottery Ticket Scanner/Lottery_Ticket_ScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lottery_Ticket_ScannerApp.swift
+//  Lottery Ticket Scanner
+//
+//  Created by Kush Patel on 4/16/26.
+//
+
+import SwiftUI
+
+@main
+struct Lottery_Ticket_ScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
